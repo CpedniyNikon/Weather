2a3e02d394ef44398b056bbce907cbb3580fc015
CpedniyNikon <50595311+CpedniyNikon@users.noreply.github.com>
2022-08-01 21:43:45 +0300

    commit

--- app/src/main/java/com/example/weather/LanguageFragment.java
+++ app/src/main/java/com/example/weather/LanguageFragment.java
@@ -4,6 +4,7 @@ package com.example.weather;
 import android.app.Activity;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,38 +21,55 @@ public class LanguageFragment extends Fragment {
     private TextView enTextView = null;
     private TextView ruTextView = null;
     private TextView uaTextView = null;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.language_theme,container,false);
-
+        return inflater.inflate(R.layout.language_theme, container, false);
     }
-
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         enTextView = (TextView) getActivity().findViewById(R.id.english_language);
         ruTextView = (TextView) getActivity().findViewById(R.id.russian_language);
         uaTextView = (TextView) getActivity().findViewById(R.id.ukrainian_language);
-        enTextView.setOnClickListener(v-> {
+        if (Locale.getDefault().getLanguage().equals("en")) {
+            enTextView.setTextColor(Color.parseColor("#017DF1"));
+        }
+        if (Locale.getDefault().getLanguage().equals("ru")) {
+            ruTextView.setTextColor(Color.parseColor("#017DF1"));
+        }
+        if (Locale.getDefault().getLanguage().equals("ua")) {
+            uaTextView.setTextColor(Color.parseColor("#017DF1"));
+        }
+        enTextView.setOnClickListener(v -> {
             setLocal(getActivity(), "en");
-            ((MainActivity)getActivity()).getSupportActionBar().setTitle(R.string.english_language);
+            ((MainActivity) getActivity()).getSupportActionBar().setTitle(R.string.english_language);
+            enTextView.setTextColor(Color.parseColor("#017DF1"));
+            ruTextView.setTextColor(Color.parseColor("#FFFFFF"));
+            uaTextView.setTextColor(Color.parseColor("#FFFFFF"));
         });
-        ruTextView.setOnClickListener(v-> {
+        ruTextView.setOnClickListener(v -> {
             setLocal(getActivity(), "ru");
-            ((MainActivity)getActivity()).getSupportActionBar().setTitle(R.string.russian_language);
+            ((MainActivity) getActivity()).getSupportActionBar().setTitle(R.string.russian_language);
+            enTextView.setTextColor(Color.parseColor("#FFFFFF"));
+            ruTextView.setTextColor(Color.parseColor("#017DF1"));
+            uaTextView.setTextColor(Color.parseColor("#FFFFFF"));
         });
-        uaTextView.setOnClickListener(v-> {
+        uaTextView.setOnClickListener(v -> {
             setLocal(getActivity(), "ua");
-            ((MainActivity)getActivity()).getSupportActionBar().setTitle(R.string.ukrainian_language);
+            ((MainActivity) getActivity()).getSupportActionBar().setTitle(R.string.ukrainian_language);
+            enTextView.setTextColor(Color.parseColor("#FFFFFF"));
+            ruTextView.setTextColor(Color.parseColor("#FFFFFF"));
+            uaTextView.setTextColor(Color.parseColor("#017DF1"));
         });
         super.onViewCreated(view, savedInstanceState);
     }
 
     public void setLocal(Activity activity, String langCode) {
         Locale locale = new Locale(langCode);
-        Resources resources =activity.getResources();
-        Configuration configuration  = resources.getConfiguration();
+        Resources resources = activity.getResources();
+        Configuration configuration = resources.getConfiguration();
         configuration.setLocale(locale);
-        resources.updateConfiguration(configuration,resources.getDisplayMetrics());
+        resources.updateConfiguration(configuration, resources.getDisplayMetrics());
     }
 }
